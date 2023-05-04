0f24a3c01edb0bc9d58bef6ff89f940ddafdd4e3
NDaly94 <NDaly94@gmail.com>
2023-05-03 20:02:56 -0400

    bottom border fix

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ function has_game_ended () {
     const hitLeftWall = snake[0].x < 0;
     const hitRightWall = snake[0].x > snakeboard.width - 20;
     const hitTopWall = snake[0].y < 0;
-    const hitBottomWall = snake[0].y > snakeboard.heaight- 20;
+    const hitBottomWall = snake[0].y > snakeboard.height- 20;
     return hitLeftWall || hitRightWall || hitBottomWall || hitTopWall;
 }    
 
